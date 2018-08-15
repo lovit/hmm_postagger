fb22c649d8752fa1162b4529e40157e4727bd02a
lovit <soy.lovit@gmail.com>
2018-08-15 23:57:02 +0900

    Training observation and transition at once

--- hmm_postagger/trainer.py
+++ hmm_postagger/trainer.py
@@ -8,8 +8,7 @@ class CorpusTrainer:
         self.verbose = verbose
 
     def train(self, corpus):
-        pos2words = self._count_pos_words(corpus)
-        transition = self._count_transition_prob(pos2words, corpus)
+        pos2words, transition = self._count_pos_words(corpus)
         return pos2words, transition
 
     def _count_pos_words(self, corpus):
@@ -17,39 +16,39 @@ class CorpusTrainer:
         def trim_words(words, min_count):
             return {word:count for word, count in words.items() if count >= min_count}
 
+        def as_bigram_tag(wordpos):
+            poslist = [pos for _, pos in wordpos]
+            return [(pos0, pos1) for pos0, pos1 in zip(poslist, poslist[1:])]
+
         pos2words = defaultdict(lambda: defaultdict(int))
+        trans = defaultdict(int)
+
+        message_format = '\rtraining observation/transition prob from %d sents'
         for i, sent in enumerate(corpus):
+            # generation prob
             for word, pos in sent:
                 pos2words[pos][word] += 1
+            # transition prob
+            for bigram in as_bigram_tag(sent):
+                trans[bigram] += 1
             if (self.verbose) and (i % 10000 == 0):
-                print('\rtraining observation prob from %d sents ...'%i, end='', flush=True)
+                print('%s ...'%(message_format%i), end='', flush=True)
         if self.verbose:
-            print('\rtraining observation prob from %d sents was done'%i)
+            print('%s was done'%(message_format%i), flush=True)
+
         pos2words = {pos:trim_words(words, self.min_count_word)
                      for pos, words in pos2words.items()}
         pos2words = {pos:words for pos, words in pos2words.items()
                      if sum(words.values()) >= self.min_count_tag}
-        return pos2words
+        trans = {pos:count for pos, count in trans.items() if pos[0] in pos2words}
 
-    def _count_transition_prob(self, tagset, corpus):
+        return pos2words, trans
 
-        def as_bigram_tag(wordpos):
-            poslist = [pos for _, pos in wordpos]
-            bigram = [(pos0, pos1) for pos0, pos1 in zip(poslist, poslist[1:])
-                      if (pos0 in tagset) and (pos1 in tagset)]
-            return bigram
-
-        trans = defaultdict(int)
-        for i, sent in enumerate(corpus):
-            for bigram in as_bigram_tag(sent):
-                trans[bigram] += 1
-            if (self.verbose) and (i % 10000 == 0):
-                print('\rtraining transition prob from %d sents ...'%i, end='', flush=True)
-        if (self.verbose):
-            print('\rtraining transition prob from %d sents was done'%i, flush=True)
-
-        base = defaultdict(int)
-        for (pos0, pos1), count in trans.items():
-            base[pos0] += count
-        trans = {pos:count/base[pos[0]] for pos, count in trans.items()}
-        return trans
+    def _to_prob(self, pos2words, transition):
+        def as_prob(trans):
+            base = defaultdict(int)
+            for (pos0, pos1), count in trans.items():
+                base[pos0] += count
+            trans = {pos:count/base[pos[0]] for pos, count in trans.items()}
+            return trans
+        raise NotImplemented
