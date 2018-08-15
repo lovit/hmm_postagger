3ee6a5ec350e2651677ebf60ebb54c4bcb1d199d
lovit <soy.lovit@gmail.com>
2018-08-15 23:20:07 +0900

    Implemented pos counting

--- hmm_postagger/trainer.py
+++ hmm_postagger/trainer.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class CorpusTrainer:
     def __init__(self, tagset=None, min_count_tag=5, min_count_word=1, verbose=True):
         self.tagset = tagset
@@ -6,4 +8,13 @@ class CorpusTrainer:
         self.verbose = verbose
 
     def train(self, corpus):
-        raise NotImplemented
+        pos2words = defaultdict(lambda: defaultdict(int))
+        for i, sent in enumerate(corpus):
+            for word, pos in sent:
+                pos2words[pos][word] += 1
+            if (self.verbose) and (i % 10000 == 0):
+                print('\rtraining from %d sents ...'%i, end='', flush=True)
+        if self.verbose:
+            print('\rtraining from %d sents was done'%i)
+        pos2words = {pos:dict(words) for pos, words in pos2words.items()}
+        return pos2words
