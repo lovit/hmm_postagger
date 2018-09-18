5071e62dbc1798e1789a56f214d96986943a7b5c
lovit <soy.lovit@gmail.com>
2018-09-18 15:52:08 +0900

    Use ford algorithm for edge list type

--- hmm_postagger/shortestpath.py
+++ hmm_postagger/shortestpath.py
@@ -1,61 +1,42 @@
-def sent_to_graph(sent, sent_len):
-    idx2node = [node for words in sent for node in words]
-    node2idx = {node:idx for idx, node in enumerate(idx2node)}
-    sent_idx = [[node2idx[word] for word in words] for words in sent]
-    sent_idx_observed = [
-        [node2idx[word] for word in words if word[3] > word[2]]
-        for words in sent]
-    edges = {}
+def _ford_list(E, V, S, T):
 
-    for b, words in enumerate(sent):
-        for word in words:
-            from_idx = node2idx[word]
-            if word[3] > sent_len: # EOS
-                continue
-            if word[3] > word[2]:
-                edges[from_idx] = sent_idx[word[3]]
-            else:
-                edges[from_idx] = sent_idx_observed[word[3]]
+    ## Initialize ##
+    # (max weight + 1) * num of nodes
+    inf = (max((weight for from_, to_, weight in E)) + 1) * len(V)
 
-    return edges, idx2node
+    # distance
+    d = {node:0 if node == S else inf for node in V}
+    # previous node
+    prev = {node:None for node in V}
 
-def ford(g, start, end):
-    from_nodes, to_nodes = _set_nodes_dict(g)
-    if not ((start in from_nodes) and (end in to_nodes)):
-        raise ValueError('There is no path {} - ... - {}'.format(start, end))
-
-    n_nodes, n_edges, cost = _initialize_dict(g, start)
-
-    for _iter in range(n_nodes * n_edges):
-        cost, changed = _update_ford_dict(g, cost)
+    ## Iteration ##
+    # preventing infinite loop
+    for _ in range(len(V)):
+        # for early stop
+        changed = False
+        for u, v, Wuv in E:
+            d_new = d[u] + Wuv
+            if d_new < d[v]:
+                d[v] = d_new
+                prev[v] = u
+                changed = True
         if not changed:
             break
-    paths = _find_shortest_path_dict(g, start, end, cost, n_nodes)
-    return {'paths': paths, 'cost': cost[end]}
 
-def _update_ford_dict(g, cost):
-    changed = False
-    for from_, to_weight in g.items():
-        for to_, weight in to_weight.items():
-            if cost[to_] > cost[from_] + weight:
-                before = cost[to_]
-                after = cost[from_] + weight
-                cost[to_] = after
-                changed = True
-    return cost, changed
+    # Checking negative cycle loop
+    for u, v, Wuv in E:
+        if d[u] + Wuv < d[v]:
+            raise ValueError('Negative cycle exists')
 
-def _set_nodes_dict(g):
-    from_nodes = set(g)
-    to_nodes = {node for nw in g.values() for node in nw.keys()}
-    return from_nodes, to_nodes
+    # Finding path
+    prev_ = prev[T]
+    if prev_ == S:
+        return {'paths':[[prev_, S][::-1]], 'cost':d[T]}
 
-def _initialize_dict(g, start):
-    nodes = set(g.keys())
-    nodes.update(set({n for nw in g.values() for n in nw.keys()}))
-    n_nodes = len(nodes)
-    n_edges = sum((len(nw) for nw in g.values()))
-    max_weight = max(w for nw in g.values() for w in nw.values())
+    path = []
+    while prev_ != S:
+        path.append(prev_)
+        prev_ = prev[prev_]
+    path.append(S)
 
-    init_cost = n_nodes * (max_weight + 1)
-    cost = {node:(0 if node == start else init_cost) for node in nodes}
-    return n_nodes, n_edges, cost
+    return {'paths':[path[::-1]], 'cost':d[T]}
